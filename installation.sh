29a3015652a77e931a5c6bad89da7472fda78045
sohrabtowfighi <sohrabtowfighi@hotmail.com>
2020-01-29 15:49:00 -0500

    matplotlib

--- installation.sh
+++ installation.sh
@@ -1,5 +1,4 @@
 sudo apt-get install python3
 sudo apt-get install python3-pip
-sudo apt-get install python3-matplotlib
 sudo apt-get install git
-pip install tqdm parmap sh matching numpy
+pip install tqdm parmap sh matching matplotlib numpy
