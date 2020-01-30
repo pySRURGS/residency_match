cf841d7a13fc01c683b70f3df2cf46d0005f08b1
sohrabtowfighi <sohrabtowfighi@hotmail.com>
2020-01-29 19:03:44 -0500

    latest version of the plotting scritp

--- plots.py
+++ plots.py
@@ -1,11 +1,18 @@
 import matplotlib.pyplot as plt
 import pandas as pd 
+import numpy as np
 from run_simulations import files
 print(files)
+plt.rcParams.update({'font.size': 8})
 box_plot_data = []
 for file in files:
     data = pd.read_csv(file)
     box_plot_data.append(data['match_rate'].values)
-plt.boxplot(box_plot_data, labels=['Baseline','Fewer\ninterviews\nper spot','More\ninterviews\nper spot','Applicants\nchoose\nspecialty\nperfectly','Applicants\napply\nto many\nspecialties'])
+median_baseline = np.median(box_plot_data[0])
+plt.figure(figsize=(4,3))
+plt.hlines(median_baseline, 0.5, 5.5, colors='b', linestyles='dashed', label='')
+plt.hlines(1, 0.5, 5.5, colors='k', linestyles='dashdot', label='')
+medianprops = dict(linestyle='-', linewidth=2.5, color='firebrick')
+plt.boxplot(box_plot_data, labels=['Baseline','Two\ninterviews\nper spot','Twenty\ninterviews\nper spot','Applicants\nchoose\nspecialty\nperfectly','Applicants\napply to 4\nspecialties'], medianprops=medianprops)
 plt.tight_layout()
 plt.show()
