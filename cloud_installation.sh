c43d0cbd311beb0b13c30a19904bb22d75405b65
pySRURGS <53630469+pySRURGS@users.noreply.github.com>
2020-03-08 11:49:45 -0400

    Update and rename installation.sh to cloud_installation.sh

---
+++ cloud_installation.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install python3
+sudo apt install python3-pip
+sudo apt install git
+git clone https://github.com/pySRURGS/residency_match.git
+cd residency_match
+pip install -r requirements.txt --user
--- installation.sh
+++
@@ -1,5 +0,0 @@
-sudo apt update
-sudo apt install python3
-sudo apt install python3-pip
-sudo apt install git
-pip3 install tqdm parmap sh matching matplotlib numpy
